c80005fd3458ee823ae2da122b759487db8d1f33
brunopirizoliveira <brunopirizoliveira@gmail.com>
2021-01-28 10:40:51 -0300

    Primeiro commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,48 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+import db from '../db.json';
+import Widget from '../components/Widget';
+import Footer from '../components/Footer';
+import GitHubCorner from '../components/GitHubCorner';
+import QuizBackground from '../components/QuizBackground';
+import QuizContainer from '../components/QuizContainer';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   text-align: -webkit-center;
+//   background-size: cover;
+//   background-position: center;
+// `;
+
+const Question = styled.div`  
+  background-image: url(${db.pokemonbg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;  
+  height: 350px;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Pokémon Quiz</h1>
+          </Widget.Header>          
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h2>Quem é esse Pokémon?</h2>
+          </Widget.Header>
+          <Widget.Content>
+            <Question></Question>
+          </Widget.Content>
+        </Widget>        
+      </QuizContainer>
+      <Footer />
+      <GitHubCorner />
+
+    </QuizBackground>
+  )
 }
